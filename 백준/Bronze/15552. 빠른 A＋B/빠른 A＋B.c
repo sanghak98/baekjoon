898dd6a12982bec20dbee529def9edb6ff0a3672
sanghak98 <ssh980307@gmail.com>
2025-04-03 15:23:43 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 300 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    int T;
+    int A, B;
+    scanf("%d", &T);
+    
+    for(i=0;i<T;i++)
+    {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A+B);
+    }
+    return 0;
+}
